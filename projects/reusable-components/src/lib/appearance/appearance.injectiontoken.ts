b3815232dde64fdaa8648ea6afe67fee20933af0
rolandomesagdp <rolandomesagdp@gmail.com>
2024-01-29 17:27:33 +0100

    Update appearance.injectiontoken.ts

--- projects/reusable-components/src/lib/appearance/appearance.injectiontoken.ts
+++ projects/reusable-components/src/lib/appearance/appearance.injectiontoken.ts
@@ -1,9 +1,5 @@
 import { InjectionToken } from "@angular/core";
 
 export const CONFIGURATOR_APPEARANCE = new InjectionToken<ConfiguratorAppearance>('squared');
-export interface ColorConfig {
-   productColor: string;
-   productGeneralConditions: string;
-}
 
 export type ConfiguratorAppearance = "squared" | "rounded"
