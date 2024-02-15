4a4b1f69a25a16ca204cb64b0d9b0b2a83b1b71d
Rolando Mesa <rmesa@toolsgroup.com>
2024-02-15 11:39:23 +0100

    Minor change

--- projects/reusable-components/src/lib/appearance/appearance.injectiontoken.ts
+++ projects/reusable-components/src/lib/appearance/appearance.injectiontoken.ts
@@ -1,7 +1,6 @@
 import { InjectionToken } from "@angular/core";
 
 export const CONFIGURATOR_APPEARANCE = new InjectionToken<ConfiguratorAppearance>('squared', {
-    providedIn: "root",
     factory: () => "squared"
 });
 
